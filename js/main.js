5b839d543812deac40430fad971d7dfca8f0214d
JonazScarpetta <tecnimundoseguridad@gmail.com>
2024-05-03 20:45:12 -0500

    3-may

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document
+  .getElementById("btn-iniciar-sesion-registro")
+  .addEventListener("click", function () {
+    window.location.href = "/html/iniciar-sesion-registro.html";
+  });
+
+document
+  .getElementById("btn-iniciar-sesion-autorizacion")
+  .addEventListener("click", function () {
+    window.location.href = "/html/iniciar-sesion-autorizador.html";
+  });
+
+document
+  .getElementById("btn-nuevo-usuario")
+  .addEventListener("click", function () {
+    window.location.href = "/html/registro.html";
+  });
