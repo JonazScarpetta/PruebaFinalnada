ca56543d282181544106d286dfafd72ae0838c42
JonazScarpetta <tecnimundoseguridad@gmail.com>
2024-05-04 07:14:03 -0500

    4-MAY

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// botones pagina inicio
 document
   .getElementById("btn-iniciar-sesion-registro")
   .addEventListener("click", function () {
@@ -15,3 +16,26 @@ document
   .addEventListener("click", function () {
     window.location.href = "/html/registro.html";
   });
+// fin de botones pagina inicio
+
+//boton de alerta//
+const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    "</div>",
+  ].join("");
+
+  alertPlaceholder.append(wrapper);
+};
+
+const alertTrigger = document.getElementById("liveAlertBtn");
+if (alertTrigger) {
+  alertTrigger.addEventListener("click", () => {
+    appendAlert("Nice, you triggered this alert message!", "success");
+  });
+}
+// fin boton de alerta
