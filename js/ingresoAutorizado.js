ed3465a0d9d5dee3fc8f0194187abdbaa40fa253
JonazScarpetta <tecnimundoseguridad@gmail.com>
2024-05-17 20:36:09 -0500

    17-may-830pm

--- js/ingresoAutorizado.js
+++ js/ingresoAutorizado.js
@@ -1,21 +1,23 @@
 const usuario = {
   nombre: "Jonathan",
   apellido: "Lopez",
+  tipoDocumento: "cedula",
   correo: "scarpetta_1985@gmail.com",
   celular: 3142976801,
+  numeroDocumento: 80927548,
 };
 
 //Nombre
 
-const contenedorNombre = document.getElementById("nombre-ingreso");
-const etiquetaNombre = document.createElement("h3"); // crear Elemento
+const contenedorNombre = document.getElementById("nombre-ingresoA");
+const etiquetaNombre = document.createElement("h6"); // crear Elemento
 
 let textoNombre = document.createTextNode(usuario.nombre); //crear el texto
 etiquetaNombre.appendChild(textoNombre); // uniendo texto con etiqueta
 contenedorNombre.appendChild(etiquetaNombre); // uniendo el contenedor don el texto
 //apellido
-const contenedorApellido = document.getElementById("apellido-ingreso");
-const etiquetaApellido = document.createElement("h3"); // crear Elemento
+const contenedorApellido = document.getElementById("apellido-ingresoA");
+const etiquetaApellido = document.createElement("h6"); // crear Elemento
 
 let textoApellido = document.createTextNode(usuario.apellido); //crear el texto
 etiquetaApellido.appendChild(textoApellido); // uniendo texto con etiqueta
@@ -23,21 +25,43 @@ contenedorApellido.appendChild(etiquetaApellido); // uniendo el contenedor don e
 
 // celular
 
-const contenedor1 = document.getElementById("numero-celular");
-const texto1 = document.createElement("h4"); // crear Elemento
+const contenedorCelular = document.getElementById("numero-celularA");
+const etiquetaCelular = document.createElement("h6"); // crear Elemento
 
-numeroCelular = document.createTextNode("3233052419"); //crear el texto
-texto1.appendChild(numeroCelular); // uniendo texto con etiqueta
-contenedor1.appendChild(texto1); // uniendo el contenedor don el texto
+let textoCelular = document.createTextNode(usuario.celular); //crear el texto
+etiquetaCelular.appendChild(textoCelular); // uniendo texto con etiqueta
+contenedorCelular.appendChild(textoCelular); // uniendo el contenedor don el texto
 
 // correo
 
-const contenedor2 = document.getElementById("correo-ingreso");
-const texto2 = document.createElement("h4"); // crear Elemento
+const contenedorCorreo = document.getElementById("correo-ingresoA");
+const etiquetaCorreo = document.createElement("h6"); // crear Elemento
 
-correoIngreso = document.createTextNode("tecnimundoseguridad@gmail.com"); //crear el texto
-texto2.appendChild(correoIngreso); // uniendo texto con etiqueta
-contenedor2.appendChild(texto2); // uniendo el contenedor don el texto
+let textoCorreo = document.createTextNode(usuario.correo); //crear el texto
+etiquetaCorreo.appendChild(textoCorreo); // uniendo texto con etiqueta
+contenedorCorreo.appendChild(textoCorreo); // uniendo el contenedor don el texto
+
+// Tipo Documento
+
+const contenedorTipoDocumento = document.getElementById(
+  "tipoDocumento-ingresoA"
+);
+const etiquetaTipoDocumento = document.createElement("h6"); // crear Elemento
+
+let textoTipoDocumento = document.createTextNode(usuario.tipoDocumento); //crear el texto
+etiquetaTipoDocumento.appendChild(textoTipoDocumento); // uniendo texto con etiqueta
+contenedorTipoDocumento.appendChild(textoTipoDocumento); // uniendo el contenedor don el texto
+
+// Numero Documento
+
+const contenedorNumeroDocumento = document.getElementById(
+  "numeroDocumento-ingresoA"
+);
+const etiquetaNumeroDocumento = document.createElement("h6"); // crear Elemento
+
+let textoNumeroDocumento = document.createTextNode(usuario.numeroDocumento); //crear el texto
+etiquetaNumeroDocumento.appendChild(textoNumeroDocumento); // uniendo texto con etiqueta
+contenedorNumeroDocumento.appendChild(textoNumeroDocumento); // uniendo el contenedor don el texto
 
 // descripcion si es usuario autroizado o registro
 
--- js/ingresoRegistro.js
+++ js/ingresoRegistro.js
@@ -27,12 +27,14 @@ const usuario = {
   apellido: "Valbuena",
   correo: "soporte.tecnico@gmail.com",
   celular: 34132976801,
+  tipoDocumento: "cedula",
+  numeroDocumento: "80927548",
 };
 
 //Nombre
 
 const contenedorNombre = document.getElementById("nombre-ingreso");
-const etiquetaNombre = document.createElement("h3"); // crear Elemento
+const etiquetaNombre = document.createElement("h6"); // crear Elemento
 
 let textoNombre = document.createTextNode(usuario.nombre); //crear el texto
 etiquetaNombre.appendChild(textoNombre); // uniendo texto con etiqueta
@@ -41,7 +43,7 @@ contenedorNombre.appendChild(etiquetaNombre); // uniendo el contenedor don el te
 //apellido
 
 const contenedorApellido = document.getElementById("apellido-ingreso");
-const etiquetaApellido = document.createElement("h3"); // crear Elemento
+const etiquetaApellido = document.createElement("h6"); // crear Elemento
 
 let textoApellido = document.createTextNode(usuario.apellido); //crear el texto
 etiquetaApellido.appendChild(textoApellido); // uniendo texto con etiqueta
@@ -50,7 +52,7 @@ contenedorApellido.appendChild(etiquetaApellido); // uniendo el contenedor don e
 // celular
 
 const contenedorCelular = document.getElementById("numero-celular");
-const etiquetaCelular = document.createElement("h4"); // crear Elemento
+const etiquetaCelular = document.createElement("h6"); // crear Elemento
 
 let textoCelular = document.createTextNode(usuario.celular); //crear el texto
 etiquetaCelular.appendChild(textoCelular); // uniendo texto con etiqueta
@@ -59,11 +61,33 @@ contenedorCelular.appendChild(etiquetaCelular); // uniendo el contenedor don el 
 // correo
 
 const contenedorCorreo = document.getElementById("correo-ingreso");
-const etiquetaCorreo = document.createElement("h4"); // crear Elemento
+const etiquetaCorreo = document.createElement("h6"); // crear Elemento
 
 let textoCorreo = document.createTextNode(usuario.correo); //crear el texto
 etiquetaCorreo.appendChild(textoCorreo); // uniendo texto con etiqueta
 contenedorCorreo.appendChild(etiquetaCorreo); // uniendo el contenedor don el texto
+
+// Tipo Documento
+
+const contenedorTipoDocumento = document.getElementById(
+  "tipoDocumento-ingreso"
+);
+const etiquetaTipoDocumento = document.createElement("h6"); // crear Elemento
+
+let textoTipoDocumento = document.createTextNode(usuario.tipoDocumento); //crear el texto
+etiquetaTipoDocumento.appendChild(textoTipoDocumento); // uniendo texto con etiqueta
+contenedorTipoDocumento.appendChild(etiquetaTipoDocumento); // uniendo el contenedor don el texto
+
+// Numero Documento Documento
+
+const contenedorNumeroDocumento = document.getElementById(
+  "numeroDocumento-ingreso"
+);
+const etiquetaNumeroDocumento = document.createElement("h6"); // crear Elemento
+
+let textoNumeroDocumento = document.createTextNode(usuario.numeroDocumento); //crear el texto
+etiquetaNumeroDocumento.appendChild(textoNumeroDocumento); // uniendo texto con etiqueta
+contenedorNumeroDocumento.appendChild(etiquetaNumeroDocumento); // uniendo el contenedor don el texto
 
 // descripcion si es usuario autroizado o registro
 
@@ -73,7 +97,7 @@ var registro = true;
 function ingresoRegistroAutorizado() {
   if (autorizado === true) {
     const contenedor3 = document.getElementById("usuarioIngresado-a-r");
-    const texto3 = document.createElement("h4"); // crear Elemento
+    const texto3 = document.createElement("h3"); // crear Elemento
 
     usuariaIngresadoAR = document.createTextNode(
       "Ingreso de Usuario Autorizado"
@@ -83,7 +107,7 @@ function ingresoRegistroAutorizado() {
   }
   if (registro === true) {
     const contenedor3 = document.getElementById("usuarioIngresado-a-r");
-    const texto3 = document.createElement("h4"); // crear Elemento
+    const texto3 = document.createElement("h3"); // crear Elemento
 
     usuariaIngresadoAR = document.createTextNode("Ingreso de Usuario registro"); //crear el texto
     texto3.appendChild(usuariaIngresadoAR); // uniendo texto con etiqueta
